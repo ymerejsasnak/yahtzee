b8b791063aa6f04cca77ac8a695a2923bbde5839
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-09-23 14:24:49 -0400

    calculates and displays all scores from rolled dice

--- yahtzee.js
+++ yahtzee.js
@@ -51,23 +51,70 @@ $(function(){
 
          //figure out how current dice would score in each category...
         this.calculate = function() {
-            var ones = this.dice.filter(function(die) { return die === 1; });
-            this.diceScore.ones = ones.length;
+            var ones = this.dice.filter(function(die) { return die === 1; }).length;
+            this.diceScore.ones = ones;
 
-            var twos = this.dice.filter(function(die) { return die === 2; });
-            this.diceScore.twos = twos.length * 2;
+            var twos = this.dice.filter(function(die) { return die === 2; }).length;
+            this.diceScore.twos = twos * 2;
 
-            var threes = this.dice.filter(function(die) { return die === 3; });
-            this.diceScore.threes = threes.length * 3;
+            var threes = this.dice.filter(function(die) { return die === 3; }).length;
+            this.diceScore.threes = threes * 3;
 
-            var fours = this.dice.filter(function(die) { return die === 4; });
-            this.diceScore.fours = fours.length * 4;
+            var fours = this.dice.filter(function(die) { return die === 4; }).length;
+            this.diceScore.fours = fours * 4;
 
-            var fives = this.dice.filter(function(die) { return die === 5; });
-            this.diceScore.fives = fives.length * 5;
+            var fives = this.dice.filter(function(die) { return die === 5; }).length;
+            this.diceScore.fives = fives * 5;
 
-            var sixes = this.dice.filter(function(die) { return die === 6; });
-            this.diceScore.sixes = sixes.length * 6;
+            var sixes = this.dice.filter(function(die) { return die === 6; }).length;
+            this.diceScore.sixes = sixes * 6;
+
+            //three of a kind
+            if (ones >= 3 || twos >= 3 || threes >= 3 || fours >= 3 || fives >= 3 || sixes >= 3) {
+                this.diceScore.threekind = this.dice.reduce(function(a, b) { return a + b; });
+            } else {
+                this.diceScore.threekind = 0;
+            }
+
+            //four of a kind
+            if (ones >= 4 || twos >= 4 || threes >= 4 || fours >= 4 || fives >= 4 || sixes >= 4) {
+                this.diceScore.fourkind = this.dice.reduce(function(a, b) { return a + b; });
+            } else {
+                this.diceScore.fourkind = 0;
+            }
+
+            //full house
+            var totals = [ones, twos, threes, fours, fives, sixes];
+            if (totals.indexOf(3) > -1 && totals.indexOf(2) > -1) {
+                this.diceScore.fullhouse = 25;
+            } else {
+                this.diceScore.fullhouse = 0;
+            }
+
+            //small straight 
+            var sortedDice = this.dice.concat().sort().toString();
+            if (sortedDice.indexOf('1,2,3,4') > -1 || sortedDice.indexOf('2,3,4,5') > -1 || sortedDice.indexOf('3,4,5,6') > -1) {
+                this.diceScore.smstraight = 30;
+            } else {
+                this.diceScore.smstraight = 0;
+            }
+
+            //large straight
+            if (sortedDice === '1,2,3,4,5' || sortedDice === '2,3,4,5,6') {
+                this.diceScore.lgstraight = 40;
+            } else {
+                this.diceScore.lgstraight = 0;
+            }
+
+            //yahtzee
+            if (totals.indexOf(5) > -1) {
+                this.diceScore.yahtzee = 50;
+            } else {
+                this.diceScore.yahtzee = 0;
+            }
+
+            //chance
+            this.diceScore.chance = this.dice.reduce(function(a, b) { return a + b; });
 
         }
         
@@ -79,6 +126,13 @@ $(function(){
             $('#dice-fours').text(this.diceScore.fours);
             $('#dice-fives').text(this.diceScore.fives);
             $('#dice-sixes').text(this.diceScore.sixes);
+            $('#dice-threekind').text(this.diceScore.threekind);
+            $('#dice-fourkind').text(this.diceScore.fourkind);
+            $('#dice-fullhouse').text(this.diceScore.fullhouse);
+            $('#dice-smstraight').text(this.diceScore.smstraight);
+            $('#dice-lgstraight').text(this.diceScore.lgstraight);
+            $('#dice-yahtzee').text(this.diceScore.yahtzee);
+            $('#dice-chance').text(this.diceScore.chance);
 
         }
         
